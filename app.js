806a672f1726b2a39c6b71668ccb803c60183b32
QuyetDong <quyetvdong@gmail.com>
2022-04-04 22:34:10 +0700

    create log stream

--- app.js
+++ app.js
@@ -1,41 +1,54 @@
 import express, { json } from "express";
-// swagger ui
 import swaggerUi from "swagger-ui-express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
-import session from 'express-session';
-
-// get json file
+import session from "express-session";
+import rfs from "rotating-file-stream";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+// get swagger json file
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const swaggerDocument = require("./src/swagger/swagger.json");
 
 import * as database from "./src/config/database.js";
-import passport from './src/middleware/passportGoogle.js';
+import passport from "./src/middleware/passportGoogle.js";
 
-const corsOptions = {
-  origin: true,
-  credentials: true,
-};
-const sessionOptions = {
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-};
+const isProduction = process.env.NODE_ENV == "production";
 
 // connect db
 database.connectDb();
 // create app
 const app = express();
-
+// helmet for security
 app.use(helmet());
-app.use(morgan('tiny'));
+// morgan
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const accessLogStream = rfs.createStream("access.log", {
+  interval: "1d",
+  path: join(__dirname, "log"),
+});
+app.use(
+  isProduction ? morgan("combined", { stream: accessLogStream }) : morgan("dev")
+);
 app.use(json());
-
+// swagger
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// cors
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
 app.use(cors(corsOptions));
+// session
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
 app.use(session(sessionOptions));
+// passport
 app.use(passport.initialize());
 app.use(passport.session());
 
